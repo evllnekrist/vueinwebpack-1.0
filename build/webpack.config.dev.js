4febdd8d264343dbc266e211f8b6dda4ff56d26a
evllnekrist <elinyine@gmail.com>
2018-10-30 17:00:01 +0700

    evoque

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -5,9 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	mode: 'development',
-	entry: [
-		'./src/index.js'
-	],
+	entry: ['./src/index.js'],
 	module: {
 		rules: [
 			{
