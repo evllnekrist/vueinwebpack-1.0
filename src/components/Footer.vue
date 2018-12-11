f6d155c7cf5df7cebb2c05d520a2e40a9fa24b0f
evllnekrist <elinyine@gmail.com>
2018-12-11 15:00:10 +0700

    ev

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<center class="almond-bar footer">
-		<i>~ this is footer ~</i>
+		<i>~ this is footer xxx ~</i>
 		<div class="almond-bold">vue-in-webpack | 2018</div>
 		<i>~ this is footer ~</i>
 	</center>
